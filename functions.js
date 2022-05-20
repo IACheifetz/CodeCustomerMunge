cc2f2d0d6d4323b17104cba8864f753ef17abd29
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-19 17:27:45 -0700

    finished first test

--- functions.js
+++ functions.js
@@ -4,8 +4,14 @@ Output:
 */
 
 export function greetUsers(customers) {
-     // just map over them to make a greeting
-    return true;
+    // just map over them to make a greeting
+    const userGreetings = customers.map(customer => {
+        return `Hello ${customer.first_name} ${customer.last_name}!`;
+
+    });
+   
+   
+    return userGreetings;
 }
 
 /* 
@@ -14,9 +20,9 @@ Output:
 */
 
 export function greetUsersOverAge60(customers) {
-    return customers
-        // first, filter over the user to get the ones over 60
-        // then map over them to make a greeting
+    return customers;
+       // first, filter over the user to get the ones over 60
+       // then map over them to make a greeting
 }
 
 
@@ -26,7 +32,7 @@ Output:
 */
 
 export function addAllAges(customers) {
-    // reduce through the customers to make a sum
+   // reduce through the customers to make a sum
     return true;
 }
 
@@ -36,19 +42,19 @@ Output:
 */
 
 export function getAverageCoolFactor(customers) {
-    // map through to make an array of cool factors
-    // then reduce through that array to get a sum
-    // then divide by the total number of customers
+   // map through to make an array of cool factors
+   // then reduce through that array to get a sum
+   // then divide by the total number of customers
     return true;
 }
 
 /* 
 Output: 
 {
-    female: 42,
-    male: 39,
-    nonbinary: 8,
-    etc . . .
+   female: 42,
+   male: 39,
+   nonbinary: 8,
+   etc . . .
 }
 */
 
@@ -58,12 +64,12 @@ export function getTotalOfEachGender(customers) {
 
 /* 
 Output: 
- {
-    female: 3,
-    male: 2,
-    nonbinary: 1,
-    etc . . .
- }
+{
+   female: 3,
+   male: 2,
+   nonbinary: 1,
+   etc . . .
+}
 */
 
 export function getGenderBreakdownOfFordOwners(customers) {
@@ -73,17 +79,17 @@ export function getGenderBreakdownOfFordOwners(customers) {
 /* 
 Output: 
 {
-    ford: {
-        female: 3,
-        male: 6,
-        nonbinary: 0,
-    },
-    mercedes:  {
-        female: 5,
-        male: 4,
-        nonbinary: 1,
-    },
-    etc . . .
+   ford: {
+       female: 3,
+       male: 6,
+       nonbinary: 0,
+   },
+   mercedes:  {
+       female: 5,
+       male: 4,
+       nonbinary: 1,
+   },
+   etc . . .
 }
 */
 
@@ -94,10 +100,10 @@ export function getGenderBreakdownOfEachCar(customers) {
 /* 
 Output: 
 {
-    ford: [3, 5, 4, 4, 7, 5],
-    mercedes: [8, 5, 6, 8, 3, 9],
-    honda: [2, 4, 4, 3, 7, 1],
-    etc. . .
+   ford: [3, 5, 4, 4, 7, 5],
+   mercedes: [8, 5, 6, 8, 3, 9],
+   honda: [2, 4, 4, 3, 7, 1],
+   etc. . .
 }
 */
 
@@ -116,9 +122,9 @@ export function getAllCoolFactorsOfEachCar(customers) {
 /* 
 Output: 
 {
-    ford: 5.4
-    mercedes:  8.5
-    honda: 2.3
+   ford: 5.4
+   mercedes:  8.5
+   honda: 2.3
 }
 */
 
@@ -132,15 +138,15 @@ Output:
 // break the customers into age demographic blocks. 
 // For example, this says there are 55 people between 10 and 19, 38 people between 20 and 29, etc
 {
-    10: 55,
-    20: 38,
-    30: 12,
-    40: 31,
-    50: 62,
-    60: 93,
-    70: 45,
-    80: 32,
-    90: 11,
+   10: 55,
+   20: 38,
+   30: 12,
+   40: 31,
+   50: 62,
+   60: 93,
+   70: 45,
+   80: 32,
+   90: 11,
 }
 */
 
@@ -153,11 +159,11 @@ Output:
 // break the customers into age blocks. 
 // For example, this lists out every cool factor for users aged between 1 and 10, then every cool factor for users aged between 21 and 30, etc
 {
-    10: [3, 5, 4, 4, 7, 5],
-    20: [8, 5, 6, 8, 3, 9],
-    30: [1, 8, 4, 1, 9, 2],
-    40: [2, 4, 4, 3, 7, 1],
-    etc . . .
+   10: [3, 5, 4, 4, 7, 5],
+   20: [8, 5, 6, 8, 3, 9],
+   30: [1, 8, 4, 1, 9, 2],
+   40: [2, 4, 4, 3, 7, 1],
+   etc . . .
 }
 */
 
@@ -172,11 +178,11 @@ Output:
 // For example, below, the average cool factor for users aged between 31-40 is 9.5.
 
 {
-    10: 5.6,
-    20: 3.1
-    30: 7.8,
-    40: 9.5,
-    etc . . .
+   10: 5.6,
+   20: 3.1
+   30: 7.8,
+   40: 9.5,
+   etc . . .
 }
 */
 
